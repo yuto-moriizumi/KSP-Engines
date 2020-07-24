ac01f4676ea873a38a46ff619dc72a470cac2133
VolgaKurvar <kurvan1112@gmail.com>
2020-07-24 16:11:58 +0900

    KSPエンジンのデータ追加 ソートが正しくできないのを修正

--- js.js
+++ js.js
@@ -1,15 +1,3 @@
 const app = new Vue({
   el: "#app",
-  data: {
-    categories: [
-      "アサルトライフル",
-      "PDW",
-      "LMG",
-      "スナイパー",
-      "カービン",
-      "DMR",
-      "ショットガン",
-      "拳銃",
-    ],
-  },
 });
